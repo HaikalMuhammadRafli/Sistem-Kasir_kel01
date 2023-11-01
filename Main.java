44561e4a5ca79ba09c7163f9d9523cbb38084c15
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-11-01 07:07:01 +0700

    added pembelian 2d array system

--- Main.java
+++ Main.java
@@ -7,22 +7,51 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String[] username = { "haikal", "irsyad", "esa"};
+        Scanner sc = new Scanner(System.in);
+
+        // array for items
+        int latestItems = 0;
+        String items[][] = new String[20][4];
+        items[0][0] = "Ayam Bakar"; // nama item
+        items[0][1] = "15000"; // harga
+        items[0][2] = "40"; // stok
+        items[0][3] = "10"; // diskon %
+
+        items[1][0] = "Es teh"; // nama item
+        items[1][1] = "3000"; // harga
+        items[1][2] = "50"; // stok
+        items[1][3] = "10"; // diskon %
+
+        // array for users
+        String[] username = { "haikal", "irsyad", "esa" };
         String[] password = { "123", "456", "789" };
-            
+
+        // array for orders
+        int latestOrders = 0;
+        String orders[][] = new String[20][5];
+        orders[0][0] = "Adi"; // nama pemesan
+        orders[0][1] = "Irsyad"; // nama kasir
+        orders[0][2] = "150000"; // subtotal
+        orders[0][3] = "50000"; // total diskon
+        orders[0][4] = "100000"; // total
+
+        // array for order details
+        int latestOrder_details = 0;
+        String order_details[][] = new String[100][7];
+        order_details[0][0] = "0"; // id
+        order_details[0][1] = "Ayam Bakar"; // nama item
+        order_details[0][2] = "10"; // jumlah beli
+        order_details[0][3] = "15000"; // harga item
+        order_details[0][4] = "150000"; // subtotal per item
+        order_details[0][5] = "50000"; // total diskon per item
+        order_details[0][6] = "100000"; // total per item
+
         boolean session = true, access = false, ordering, stocking;
         String inputUsername, inputPassword;
         int choice1, choice2, choice3;
-        double total = 0, subTotal;
+
         String item[] = { "Ayam Bakar", "Es Teh" };
         int stok[] = { 40, 50 };
-        // String username = "admin", password = "admin123";
-        // int hargaAyamBakar = 13000;
-        // int hargaEsTeh = 3000;
-        // int stokAyamBakar = 40;
-        // int stokEsTeh = 50;
-
-        Scanner sc = new Scanner(System.in);
 
         while (session == true) {
             // login
@@ -34,20 +63,26 @@ public class Main {
                 inputPassword = sc.nextLine();
 
                 // TODO: ganti menggunakan array username dan password
-                
-                    for (int i = 0; i < username.length; i++) {
-                        if (inputUsername.equals(username[i]) && inputPassword.equals(password[i]) ) {
-                            System.out.println();
-                            System.out.println("login berhasil");
-                            access = true;
-                            break;
-                        }
-                    }
-                  
-        
-            }
-            // menu
 
+                for (int i = 0; i < username.length; i++) {
+                    if (inputUsername.equals(username[i]) && inputPassword.equals(password[i])) {
+                        System.out.println();
+                        System.out.println("login berhasil");
+                        access = true;
+                        break;
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < items.length; i++) {
+                if (items[i][0] == null) {
+                    latestItems = i;
+                    break;
+                }
+            }
+
+            // menu
             System.out.println();
             System.out.println("Cafe The Orange!");
             System.out.println("Choose 1 to make an order");
@@ -55,58 +90,120 @@ public class Main {
             System.out.println("Choose 3 to exit the program");
             System.out.print("Input your answer : ");
             choice1 = sc.nextInt();
+            sc.nextLine();
+            System.out.println();
 
             switch (choice1) {
                 case 1:
 
+                    for (int i = 0; i < orders.length; i++) {
+                        if (orders[i][0] == null) {
+                            latestOrders = i;
+                            break;
+                        }
+                    }
+                    System.out.println(latestOrders);
+
+                    System.out.print("Nama Pembeli : ");
+                    orders[latestOrders][0] = sc.nextLine();
+                    orders[latestOrders][1] = "Irsyad";
+
                     ordering = true;
                     while (ordering == true) {
+
+                        for (int i = 0; i < order_details.length; i++) {
+                            if (order_details[i][0] == null) {
+                                latestOrder_details = i;
+                                break;
+                            }
+                        }
+
                         System.out.println("Cafe The Orange Menu!");
-                        // TODO: Gunakan looping dan array
-                        // System.out.println("Pilih 1 untuk pesan Ayam Bakar (" + stokAyamBakar + ")");
-                        // System.out.println("Pilih 2 untuk pesan Es Teh (" + stokEsTeh + ")");
-                        System.out.println("Pilih 3 untuk selesai");
-                        System.out.println("");
-                        System.out.println("Total : Rp " + total);
-                        System.out.println("");
-                        System.out.print("Pilihan : ");
+
+                        for (int i = 0; i < items.length; i++) {
+                            if (items[i][0] != null) {
+                                System.out.println("[" + (i + 1) + "] Order " + items[i][0] + " (" + items[i][2] + ")");
+                            }
+                        }
+
+                        System.out.println("[" + items.length + "] to exit\n");
+                        System.out.println("List pembelian :");
+
+                        for (int i = 0; i < order_details.length; i++) {
+                            if (order_details[i][0] != null
+                                    && order_details[i][0].equals(Integer.toString(latestOrders))) {
+                                System.out.print(order_details[i][0]);
+                                System.out.println("> " + order_details[i][1] + " (" + order_details[i][2] +
+                                        ")");
+                            }
+                        }
+                        System.out.println();
+
+                        // subtotal
+                        orders[latestOrders][2] = "0";
+                        for (int i = 0; i < order_details.length; i++) {
+                            if (order_details[i][0] != null
+                                    && order_details[i][0].equals(Integer.toString(latestOrders))) {
+                                orders[latestOrders][2] = Integer.toString(Integer.parseInt(orders[latestOrders][2])
+                                        + Integer.parseInt(order_details[i][4]));
+                            }
+                        }
+
+                        // total diskon
+                        orders[latestOrders][3] = "0";
+
+                        // total
+                        orders[latestOrders][4] = "0";
+                        orders[latestOrders][4] = Integer.toString(Integer.parseInt(orders[latestOrders][2])
+                                - Integer.parseInt(orders[latestOrders][3]));
+
+                        System.out.println("Total : Rp " + orders[latestOrders][4] + "\n");
+                        System.out.print("Choice : ");
 
                         choice2 = sc.nextInt();
 
-                        switch (choice2) {
-                            case 1:
-                                System.out.println("Beli Berapa PCS (Ayam Bakar)?");
-                                int jumlahAyamBakar = sc.nextInt();
-                                // Todo: Gunakan perhitungan array
-                                // subTotal = hargaAyamBakar * jumlahAyamBakar;
-                                // total += subTotal;
-                                // subTotal = 0;
-                                // stokAyamBakar -= jumlahAyamBakar;
-                                break;
+                        if (items[choice2 - 1][0] != null) {
 
-                            case 2:
-                                System.out.println("Beli Berapa PCS (Es Teh)?");
-                                int jumlahEsTeh = sc.nextInt();
-                                // Todo: Gunakan perhitungan array
-                                // subTotal = hargaEsTeh * jumlahEsTeh;
-                                // total += subTotal;
-                                // subTotal = 0;
-                                // stokEsTeh -= jumlahEsTeh;
-                                break;
+                            order_details[latestOrder_details][0] = Integer.toString(latestOrders); // id
 
-                            case 3:
-                                System.out.println("Thank you!");
-                                System.out.println("Your total : " + total);
-                                ordering = false;
-                                break;
+                            order_details[latestOrder_details][1] = items[choice2 - 1][0]; // nama
 
-                            default:
-                                System.out.println("Menu tersebut tidak ada!");
-                                System.out.println("Silahkan coba lagi!");
-                                break;
+                            System.out.print("Beli Berapa PCS " + items[choice2 - 1][1] + " ? : ");
+                            order_details[latestOrder_details][2] = Integer.toString(sc.nextInt()); // jumlah
+
+                            order_details[latestOrder_details][3] = items[choice2 - 1][1]; // harga
+
+                            order_details[latestOrder_details][4] = Integer.toString(Integer
+                                    .parseInt(order_details[latestOrder_details][3])
+                                    * Integer.parseInt(order_details[latestOrder_details][2])); // subtotal
+
+                            order_details[latestOrder_details][5] = "0"; // total diskon
+
+                            order_details[latestOrder_details][6] = Integer
+                                    .toString(Integer.parseInt(order_details[latestOrder_details][4])
+                                            - Integer.parseInt(order_details[latestOrder_details][5])); // total
+
+                            items[choice2 - 1][2] = Integer.toString(Integer.parseInt(items[choice2 - 1][2])
+                                    - Integer.parseInt(order_details[latestOrder_details][2])); // pengurangan stok
+
+                            continue;
+
+                        } else if (choice2 == items.length) {
+
+                            System.out.println("Thank you!");
+                            System.out.println("Your total : " + orders[latestOrders][4]);
+                            ordering = false;
+                            break;
+
+                        } else {
+                            System.out.println("Menu tersebut tidak ada!");
+                            System.out.println("Silahkan coba lagi!");
+                            continue;
+
                         }
                     }
-                    break;
+
+                    continue;
 
                 case 2:
                     stocking = true;
@@ -118,10 +215,7 @@ public class Main {
                             System.out.println(
                                     "Pilih " + (i + 1) + " untuk tambah stok " + item[i] + " (" + stok[i] + ")");
                         }
-
-                        System.out.println("Pilih 1 untuk tambah stok Ayam Bakar (" + stok[0] + ")");
-                        System.out.println("Pilih 2 untuk tambah stok Es Teh (" + stok[1] + ")");
-                        System.out.println("Pilih 5 untuk selesai");
+                        System.out.println("Pilih 3 untuk selesai");
                         System.out.println("");
                         System.out.print("Pilihan : ");
 
