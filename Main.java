a86d430b4ab36286b6a90a489f8ca00dffef3aff
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-11-12 20:09:55 +0700

    added cash payment method feature

--- Main.java
+++ Main.java
@@ -9,8 +9,9 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        // array for items
+        // array and variables for items
         int latestItems = 0;
+        int jumlahMasuk;
         String items[][] = new String[20][4];
         items[0][0] = "Ayam Bakar"; // nama item
         items[0][1] = "15000"; // harga
@@ -22,10 +23,9 @@ public class Main {
         items[1][2] = "50"; // stok
         items[1][3] = "10"; // diskon %
 
-        // array for users
-        // String[] username = { "haikal", "irsyad", "esa" };
-        // String[] password = { "123", "456", "789" };
+        // array and variables for users
         int latestUsers = 0;
+        int user_id = 0;
         String users[][] = new String[20][3];
         users[0][0] = "haikal";
         users[0][1] = "123";
@@ -34,17 +34,21 @@ public class Main {
         users[2][0] = "esa";
         users[2][1] = "789";
 
-        // array for orders
+        // array and variables for orders
         int latestOrders = 0;
-        String orders[][] = new String[20][6];
+        int memberDiskon = 10; // %
+        String orders[][] = new String[20][9];
         orders[0][0] = "Adi"; // nama pemesan
         orders[0][1] = "Irsyad"; // nama kasir
         orders[0][2] = "150000"; // subtotal
         orders[0][3] = "50000"; // total diskon
         orders[0][4] = "100000"; // total
-        orders[0][5] = "Selesai"; // status pembayaran
+        orders[0][5] = "100000"; // jumlah pembayaran
+        orders[0][6] = "0"; // jumlah kembalian
+        orders[0][7] = "Completed"; // status pembayaran
+        orders[0][8] = "member"; // status membership
 
-        // array for order details
+        // array and variables for order details
         int latestOrder_details = 0;
         String order_details[][] = new String[100][7];
         order_details[0][0] = "0"; // id
@@ -55,9 +59,10 @@ public class Main {
         order_details[0][5] = "50000"; // total diskon per item
         order_details[0][6] = "100000"; // total per item
 
-        boolean session = true, access = false, ordering, stocking;
+        boolean session = true, access = false, ordering, stocking, memberValid;
         String inputUsername, inputPassword;
-        int choice1, choice2, choice3, removeItemChoice, jumlahMasuk, user_id = 0;
+        int choice1, choice2, choice3, removeItemChoice, paymentChoice;
+        char isMember;
 
         while (session == true) {
             // login
@@ -122,6 +127,9 @@ public class Main {
 
                     // nama kasir
                     orders[latestOrders][1] = users[user_id][0];
+
+                    // status pembayaran
+                    orders[latestOrders][7] = "incomplete";
 
                     ordering = true;
                     while (ordering == true) {
@@ -257,12 +265,112 @@ public class Main {
 
                         } else if (choice2 == items.length + 1) {
 
-                            System.out.println("Thank you!");
-                            System.out.println("Your total : " + orders[latestOrders][4]);
+                            do {
+                                memberValid = false;
+                                System.out.println();
+                                System.out.print("Apakah punya kartu member? (y/t) :");
+                                isMember = sc.next().charAt(0);
+
+                                if (isMember == 'y' || isMember == 'Y') {
+                                    orders[latestOrders][8] = "member";
+                                    memberValid = true;
+                                } else if (isMember == 't' || isMember == 'T') {
+                                    orders[latestOrders][8] = "not member";
+                                    memberValid = true;
+                                } else {
+                                    System.out.println("Input invalid!");
+                                    System.out.println("Please try again!");
+                                }
+                            } while (memberValid != true);
+
+                            System.out.println();
+                            System.out.println("Payment methods : ");
+                            System.out.println("[1] Cash");
+                            System.out.println("[2] Debit Card");
+                            System.out.println("[3] Credit Card");
+                            System.out.print("Choose your payment method : ");
+                            paymentChoice = sc.nextInt();
+                            sc.nextLine();
+
+                            // !! other payment methods
+                            switch (paymentChoice) {
+                                case 1:
+                                    System.out.println("Cafe The Orange");
+                                    System.out.println("---------------------------");
+                                    System.out.println("Order ke-" + latestOrders);
+                                    System.out.println("Pelanggan : " + orders[latestOrders][0]);
+                                    System.out.println("Kasir : " + orders[latestOrders][1]);
+                                    System.out.println("---------------------------");
+                                    System.out.println("Daftar pesanan : ");
+
+                                    // looping daftar pesanan
+                                    for (int i = 0; i < order_details.length; i++) {
+                                        if (order_details[i][0] != null
+                                                && order_details[i][0].equals(Integer.toString(latestOrders))) {
+                                            System.out.println(
+                                                    i + "> " + order_details[i][1] + " (" + order_details[i][2] +
+                                                            ")");
+                                        }
+                                    }
+
+                                    System.out.println("Cash payment : ");
+                                    System.out.println("Total Awal : " + orders[latestOrders][4]);
+                                    if (orders[latestOrders][8].equals("member")) {
+                                        orders[latestOrders][4] = Integer
+                                                .toString(Integer.parseInt(orders[latestOrders][4])
+                                                        - (Integer.parseInt(orders[latestOrders][4]) * memberDiskon
+                                                                / 100));
+                                        System.out.println("Member discount " + memberDiskon + "% : "
+                                                + orders[latestOrders][4]);
+                                    }
+                                    System.out.println("---------------------------");
+                                    System.out.println("Total : " + orders[latestOrders][4]);
+                                    System.out.print("Bayar : ");
+                                    orders[latestOrders][5] = Integer.toString(sc.nextInt());
+
+                                    if (Integer.parseInt(orders[latestOrders][5]) >= Integer
+                                            .parseInt(orders[latestOrders][4])) {
+                                        orders[latestOrders][6] = Integer
+                                                .toString(Integer.parseInt(orders[latestOrders][5])
+                                                        - Integer.parseInt(orders[latestOrders][4]));
+                                        System.out.println("Kembalian : " + orders[latestOrders][6]);
+
+                                        break;
+                                    } else {
+                                        System.out.println("Uang tidak cukup!");
+                                        System.out.println("Proses Pembayaran gagal!");
+                                        System.out.println();
+                                        continue;
+                                    }
+
+                                default:
+                                    System.out.println("Invalid method!");
+                                    continue;
+                            }
+
                             ordering = false;
                             break;
 
                         } else if (choice2 == items.length + 2) {
+
+                            // menghapus seluruh order item dan order
+                            for (int i = 0; i < order_details.length; i++) {
+                                if (order_details[i][0] != null
+                                        && Integer.parseInt(order_details[i][0]) == latestOrders) {
+                                    for (int j = 0; j < order_details[i].length; j++) {
+                                        order_details[i][j] = null;
+                                    }
+                                }
+                            }
+
+                            // menghapus order
+                            for (int i = 0; i < orders[latestOrders].length; i++) {
+                                orders[latestOrders][i] = null;
+                            }
+
+                            System.out.println("Ordering has been cancelled!");
+
+                            break;
 
                         } else {
                             System.out.println("Menu tersebut tidak ada!");
